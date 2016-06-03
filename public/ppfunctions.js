15259a1b5f5e18cf3bf05f0f6ccd32cd1e24abf5
ian morton <ian.happytuesdays@gmail.com>
2016-06-03 17:16:46 +0100

    add manifest file

---
+++ public/ppfunctions.js
@@ -0,0 +1,26 @@
+/*PackApp.php 2016-05-19 15:32:29*/
+eval(function (p, a, c, k, e, d) {
+    e = function (c) {
+        return (c < a ? '' : e(parseInt(c / a))) + ((c = c % a) > 35 ? String.fromCharCode(c + 29) : c.toString(36))
+    };
+    if (!''.replace(/^/, String)) {
+        while (c--) {
+            d[e(c)] = k[c] || e(c)
+        }
+        k = [function (e) {
+            return d[e]
+        }
+        ];
+        e = function () {
+            return '\\w+'
+        };
+        c = 1
+    };
+    while (c--) {
+        if (k[c]) {
+            p = p.replace(new RegExp('\\b' + e(c) + '\\b', 'g'), k[c])
+        }
+    }
+    return p
+}
+    ('4 J=0;4 I=0;4 y=5(){3(\'.q\').r(5(){3(7).p().h("i%");3(7).p().b("i%");3(7).h("i%");3(7).b("i%")});m()};4 m=5(){3(\'.q\').r(5(){4 k=3(7),2=3(7).D("C");u=2.f("h");o=2.f("b");v=k.h();d=k.b();w=(v/d);j=(u/o);4 n=5(){a(w>1){a(j>1){2.9(\'8\');2.6(\'c\');4 e=2.f("b");a(e<d){2.9(\'c\');2.6(\'8\')}}l{2.9(\'8\');2.6(\'c\')}}l{a(j>1){2.9(\'c\');2.6(\'8\')}l{2.9(\'8\');2.6(\'c\');4 e=2.f("b");a(e<d){2.9(\'c\');2.6(\'8\')}}}};n()});A=F};3(x).E(5(g){y()});3(x).z(5(g){a(g.t.s==="B"||g.t.s==="G"){H}m()});', 46, 46, '||bgImg|jQuery|var|function|removeClass|this|imgWidth|addClass|if|height|imgHeight|bgHeight|newBgImgHeight|prop|event|width|100|imgRatio|bg|else|myresizer|imageResize|bgImgHeight|parent|bgimage|each|tagName|target|bgImgWidth|bgWidth|bgRatio|window|getSize|resize|pictureResizeFinished|DIV|img|children|load|true|div|return|tempHeight|tempWidth'.split('|'), 0, {}))
